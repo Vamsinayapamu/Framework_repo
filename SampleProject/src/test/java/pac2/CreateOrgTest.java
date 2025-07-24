9030367da7dae9caefdf2e6a6df36b28b8a29fed
Vamsiname <vamsi964284@gmail.com>
2025-07-24 12:31:47 +0530

    first commit

---
+++ SampleProject/src/test/java/pac2/CreateOrgTest.java
@@ -0,0 +1,19 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("executed createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("executed modifyOrgTest");
+	}
+
+}
